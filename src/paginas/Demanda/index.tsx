4ee53302dafc859c79a0cff13628ed2f068d037c
Gonzalo Pozzoli <gonzalopozzoli2@gmail.com>
2024-06-06 15:46:59 -0300

    pantallas base de orden y demanda

--- src/paginas/Demanda/index.tsx
+++ src/paginas/Demanda/index.tsx
@@ -1,7 +1,33 @@
-
+import TablaDeDatos, { PropsTablaDeDatos } from "../../componentes/Tabla";
+import styles from "./styles.module.scss";
 
 export default function PaginaDemanda() {
-    return (
-        <div></div>
-    )
+  const columnasTabla: PropsTablaDeDatos<{
+    id: number;
+    fechaDesde: Date;
+    fechaHasta: Date;
+    articulo: string;
+  }>["columnas"] = [
+    {
+      nombreMostrado: "Articulo",
+      elementoMostrado: (demanda) => demanda.articulo,
+    },
+    {
+      nombreMostrado: "Rango fecha",
+      elementoMostrado: (demanda) =>
+        `${demanda.fechaDesde.toLocaleString()} - ${demanda.fechaHasta.toLocaleString()}`,
+    },
+  ];
+  return (
+    <div>
+      <div className={styles["contenedor-tabla"]}>
+        <TablaDeDatos
+          columnas={columnasTabla}
+          filas={[
+            { id: 1, articulo: "Articulo 1", fechaDesde: new Date(), fechaHasta: new Date() },
+          ]}
+        />
+      </div>
+    </div>
+  );
 }
--- src/paginas/OrdenCompra/index.tsx
+++ src/paginas/OrdenCompra/index.tsx
@@ -1,7 +1,34 @@
-
+import TablaDeDatos, { PropsTablaDeDatos } from "../../componentes/Tabla";
+import styles from "./styles.module.scss";
 
 export default function PaginaOrdenesDeCompra() {
-    return (
-        <div></div>
-    )
+  const columnasTabla: PropsTablaDeDatos<{
+    id: number;
+    cantidad: number;
+    fechaOrden: Date;
+    articulo: string;
+  }>["columnas"] = [
+    {
+      nombreMostrado: "Articulo",
+      elementoMostrado: (orden) => orden.articulo,
+    },
+    {
+      nombreMostrado: "Cantidad",
+      elementoMostrado: (orden) => orden.cantidad,
+    },
+    {
+      nombreMostrado: "Fecha pedido",
+      elementoMostrado: (orden) => orden.fechaOrden.toLocaleString(),
+    },
+  ];
+  return (
+    <div>
+      <div className={styles["contenedor-tabla"]}>
+        <TablaDeDatos
+          columnas={columnasTabla}
+          filas={[{ id: 1, articulo: "Articulo 1", cantidad: 10, fechaOrden: new Date() }]}
+        />
+      </div>
+    </div>
+  );
 }
